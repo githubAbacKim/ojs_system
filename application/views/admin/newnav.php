e9f5d867a972b087414e4e25b65e66f626759801
Kim Abac <sharkwebbuilder@gmail.com>
2019-02-20 23:17:08 +0800

    feb latest update

--- application/views/admin/newnav.php
+++ application/views/admin/newnav.php
@@ -3,6 +3,12 @@
         <nav class="navbar navbar-default navbar-static-top" role="navigation" style="border-bottom:0;margin-bottom: 0;">
             <div class="navbar-header">
                 <a class="navbar-brand" href="index.html">SB Admin v2.0</a>
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
             </div>
             <!-- /.navbar-header -->
             <ul class="nav navbar-top-links navbar-right">
@@ -21,24 +27,23 @@
             <!-- /.navbar-top-links -->
 
         </nav>
-
-
-
-        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
-            <div class="col-lg-6 col-lg-offset-3">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <ul class="nav navbar-top-links navbar-right text-center">
+        <style scoped="true">
+            .topnav{
+                z-index: 100 !important;
+                border-bottom:1px solid #e7e7e7;
+            }
+            .menu{
+                z-index: 1000 !important;
+            }
+        </style>
+        <div class="col-lg-12 topnav">
+            <div class="sidebar-nav navbar-collapse">
+                <ul class="menu nav navbar-top-links navbar-center text-center">
                 <li class="dropdown">
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#">
                         Account Settings  <i class="fa fa-caret-down"></i>
                     </a>
-                    <ul class="dropdown-menu dropdown-user">
+                    <ul class="dropdown-menu">
                         <li>
                             <a href="<?php echo base_url("")?>">Account Info</a>
                         </li>
@@ -53,7 +58,7 @@
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#">
                         Manage Employee <i class="fa fa-caret-down"></i>
                     </a>
-                    <ul class="dropdown-menu dropdown-user">
+                    <ul class="dropdown-menu">
                         <li>
                             <a href="<?php echo base_url("")?>">Salary</a>
                         </li>
@@ -76,7 +81,7 @@
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#">
                         Manage Stocks  <i class="fa fa-caret-down"></i>
                     </a>
-                    <ul class="dropdown-menu dropdown-user">
+                    <ul class="dropdown-menu">
                         <li>
                             <a href="<?php echo base_url("")?>">Stock Class</a>
                         </li>
@@ -95,20 +100,56 @@
                     <a class="dropdown-toggle" data-toggle="dropdown" href="#">
                         Manage Expenses  <i class="fa fa-caret-down"></i>
                     </a>
-                    <ul class="dropdown-menu dropdown-user">
+                    <ul class="dropdown-menu">
                         <li>
-                            <a href="<?php echo base_url("")?>">Account Info</a>
+                            <a href="<?php echo base_url("")?>">Miscellanous</a>
                         </li>
                         <li class="divider"></li>
                         <li>
-                            <a href="<?php echo base_url("")?>">Account Security</a>
+                            <a href="<?php echo base_url("")?>">Product</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <a href="<?php echo base_url("")?>">Equipment</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <a href="<?php echo base_url("")?>">Returnable</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <a href="<?php echo base_url("")?>">Stocks</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <a href="<?php echo base_url("")?>">Salary</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <a href="<?php echo base_url("")?>">Overtime</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <a href="<?php echo base_url("")?>">Attendance</a>
                         </li>
                     </ul>
                     <!-- /.dropdown-messages -->
                 </li>
-
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        Manage Reports  <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a href="<?php echo base_url("")?>">Sales</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <a href="<?php echo base_url("")?>">Income Statement</a>
+                        </li>
+                    </ul>
+                    <!-- /.dropdown-messages -->
+                </li>
             </ul>
             </div>
-            </div>
-            <!-- /.navbar-header -->
-        </nav>
+        </div>
--- application/views/admin/testpanel.php
+++ application/views/admin/testpanel.php
@@ -1,7 +1,22 @@
+<style scope>
+    #property-box,#setting-box,
+    #check-in-box,
+    #floormanagement-box,
+    #new-box,#edit-floor,#void-box{
+    position: relative;
+    /*padding: 20px;*/
+    width:auto;
+    max-width: 600px;
+    margin: 20px auto !important;
+    }
+</style>
 <div class="col-lg-8 col-lg-offset-2">
   <div class="row">
       <div class="col-lg-12">
           <h1 class="text-left"><?php echo $sub_heading;?></h1>
+      </div>
+      <div class="col-lg-12">
+
       </div>
   </div>
 </div>
